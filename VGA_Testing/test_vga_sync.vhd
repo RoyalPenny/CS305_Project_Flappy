19c2bb9b2ce9855b927783ddc9f81372d7d53954
RoyalPenny <100495516+RoyalPenny@users.noreply.github.com>
2023-05-05 15:01:49 +1200

    Added psudo randomizer

--- VGA_Testing/test_vga_sync.vhd
+++ VGA_Testing/test_vga_sync.vhd
@@ -13,6 +13,7 @@ architecture TEST of TEST_VGA_SYNC is
     signal t_red, t_green, t_blue, t_clkDiv, t_vert_sync_out, t_horiz_sync_out: STD_LOGIC;
     signal t_pixel_row: STD_LOGIC_VECTOR(9 DOWNTO 0);
     signal t_pixel_column: STD_LOGIC_VECTOR(9 DOWNTO 0);
+    signal t_psudo_rand: std_logic_vector(8 downto 0);
 
     component VGA_SYNC is
 	   PORT(	clock_25Mhz, red, green, blue		: IN	STD_LOGIC;
@@ -31,6 +32,11 @@ architecture TEST of TEST_VGA_SYNC is
               clkDiv : out std_logic);
     end component;
     
+    component Psudo_Gen is
+   	  PORT(clk, rst : IN std_logic;
+		   	      psudo_rand : OUT std_logic_vector(8 downto 0));	
+		end component;
+    
     begin   
     -- generate signals:
     vert_sync_out <= t_vert_sync_out;
@@ -43,5 +49,7 @@ architecture TEST of TEST_VGA_SYNC is
 	  ball_design: bouncy_ball port map (pb1, pb2, t_clkDiv, t_vert_sync_out, t_pixel_row, t_pixel_column, t_red, t_green, t_blue);   
 	  
 	  div_design: Div port map (CLK, t_clkDiv);
+	    
+	  psudo_rand_design: Psudo_Gen port map(t_clkDiv, pb2, t_psudo_rand);
 	    
   end architecture;
