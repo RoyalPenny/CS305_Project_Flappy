fd8117d0ba5f7f29aa3287af6125ad5f66dc009f
Andy Woo <dngusaud3969@gmail.com>
2023-05-11 02:20:26 +1200

    text_Test_case done  Andy

--- Text_Testing/sprite_printer.vhd
+++ Text_Testing/sprite_printer.vhd
@@ -3,25 +3,25 @@ USE ieee.std_logic_1164.all;
 USE IEEE.STD_LOGIC_ARITH.all;
 USE IEEE.STD_LOGIC_UNSIGNED.all;
 
-entity TEXT_PRINTER is
+entity SPRITE_PRINTER is
   
   -- pixel_row, pixel_col : current pixel row and column
   -- anchor_row, anchor_col : where to place the text, the top left is the anchor of the 8x8 text tile
-  -- character: what character to display
+  -- address: what character to display, please provide its address from the .mif file
   -- rom_mux_out : once character and position is set, it will send 1 or 0, according to current row and col pixel.
   
   port(pixel_row, pixel_col, anchor_row, anchor_col : in std_logic_vector(9 downto 0);
        multiplier : in std_logic_vector(2 downto 0);
-       character : in std_logic_vector (5 downto 0);
-       clk : in std_logic;
+       address : in std_logic_vector (5 downto 0);
+       enable, clk : in std_logic;
        rom_mux_output : out std_logic);
   
-end TEXT_PRINTER;
+end SPRITE_PRINTER;
 
-architecture a of TEXT_PRINTER is
+architecture a of SPRITE_PRINTER is
   
-  signal s_font_row : std_logic_vector(2 downto 0);
-  signal s_font_col : std_logic_vector(2 downto 0);
+  signal s_font_row : std_logic_vector(2 downto 0) := "000"; --row 0 is the top row
+  signal s_font_col : std_logic_vector(2 downto 0) := "000"; --col 0 is the far right col
   
   component CHAR_ROM is 
     port (character_address	:	in std_logic_vector (5 downto 0);
@@ -31,9 +31,11 @@ architecture a of TEXT_PRINTER is
 	end component;
   
 begin
-  
-  character_rom:CHAR_ROM port map (character_address => character, font_row => s_font_row, font_col => s_font_col,  clock => clk, rom_mux_output => rom_mux_output);
-  
 
+  SPRITE_ROM:CHAR_ROM port map (character_address => address, font_row => s_font_row, font_col => s_font_col,  clock => clk, rom_mux_output => rom_mux_output);
+  
+  --s_font_row <= (pixel_row - anchor_row)(3) when pixel_row >= anchor_row and pixel_row <= anchor_row + 8;
+  --s_font_col <= pixel_row - anchor_row when pixel_col >= anchor_col and pixel_col <= anchor_col + 8;
+  
 end a;
        
