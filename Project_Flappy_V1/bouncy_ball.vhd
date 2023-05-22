b1e8577ac7bb4bafd7ba87e584683760fdbd365c
RoyalPenny <100495516+RoyalPenny@users.noreply.github.com>
2023-05-22 16:40:15 +1200

    Completed collision detection

--- Project_Flappy_V1/bouncy_ball.vhd
+++ Project_Flappy_V1/bouncy_ball.vhd
@@ -6,7 +6,7 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 ENTITY bouncy_ball IS
 	PORT
-		( pb1, enable, clk, vert_sync	: IN std_logic;
+		( pb1, enable, clk, vert_sync : IN std_logic;
           pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
 		  red, green, blue, ball_state 			: OUT std_logic);		
 END bouncy_ball;
@@ -27,10 +27,11 @@ size <= CONV_STD_LOGIC_VECTOR(8,10);
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(320,11);
 
 ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
-			'0';
+					 and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+			     '0';
 
-ball_state <= ball_on;
+ball_state <= ball_on when (pixel_column > conv_std_logic_vector(311, 10) and pixel_column < conv_std_logic_vector(329, 10)) else
+              '0';
 
 
 -- Colours for pixel data on video signal
@@ -39,7 +40,7 @@ Red <= '1';
 Green <= not ball_on;
 Blue <= not ball_on;
 
-Move_Ball: process (vert_sync, enable)  	
+Move_Ball: process (vert_sync)  	
 begin
 	-- Move ball once every vertical sync
 	 
